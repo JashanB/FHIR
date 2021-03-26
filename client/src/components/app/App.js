89a1926b0874607fdf63a4183be73ac52bca5c38
JashanB <jashan_brar50@hotmail.com>
2021-03-25 22:14:45 -0400

    added react structure and router

---
+++ client/src/components/app/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import back from './back.png';
+
+function App() {
+  const [offHome, setOffHome] = useState(false);
+  //using react router
+
+  //main landing page, 2 buttons to click to histogram and then graph (both full page elements on diff routes)
+  //make axios call here, pass state down to children
+  return (
+    <Router>
+      <div className="App">
+        <div className="header">
+          <img alt="" src={"https://seamless.md/wp-content/uploads/2019/03/Artboard-1@4x-166x37.png"}></img>
+          {offHome && <Link to="/"><img alt="" src={back}></img>Back</Link>}
+        </div>
+        <Switch>
+          <Route path='/histogram' render={(props) => <Histogram {...props} setOffHome={setOffHome}/>} />
+          <Route path='/table' render={(props) => <Table {...props} setOffHome={setOffHome}/>} />
+          <Route path='/stats' render={(props) => <Stats {...props} setOffHome={setOffHome}/>} />
+          <Route path='/' render={(props) => <Home {...props} />} setOffHome={setOffHome}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,25 @@
+import './Home.css';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home(props) {
+  //3 buttons to stats, table, histogram
+  useEffect(() => {
+    setOffHome(state => false);
+  }, [])
+  return (
+    <div className="main-container">
+      <div className="home-container">
+        <div className="home-button">
+          <span>Statistics</span>
+        </div>
+        <div className="home-button">
+          <span>Histogram</span>
+        </div>
+        <div className="home-button">
+          <span>Table</span>
+        </div>
+      </div>
+    </div>
+  )
+}
